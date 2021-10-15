0c0b3e74078b71b8e0fb9013904b4f4458e835aa
bdavidbm <bdbastidas88@gmail.com>
2021-10-15 01:55:20 -0500

    Creacion de tabla de productos, agregar y eliminar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Articulo } from './articulo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'productos';
+  titulo = 'Administración de artículos';
+  codigoInput: number=0;
+  descripcionInput: string='';
+  precioInput: number=0;
+
+  articulos: Articulo[]= [ new Articulo(1,'papas',1500)];
+
+  agregarArticulo(){
+    let articuloN=new Articulo(this.codigoInput, this.descripcionInput, this.precioInput);
+    this.articulos.push(articuloN)
+  }
+
+  borrarArticulo(codigoB: number){
+    this.articulos.forEach(function(articuloB, index, obj){
+      if(articuloB.codigo === codigoB){
+        obj.splice(index,1);
+      }
+    })
+  }
 }
