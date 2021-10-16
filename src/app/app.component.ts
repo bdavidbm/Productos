ae5a6682c2ac30f55095123ef3d9f71314149662
bdavidbm <bdbastidas88@gmail.com>
2021-10-16 02:38:52 -0500

    Se agrego boton de modificacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,18 @@ export class AppComponent {
   codigoInput: number=0;
   descripcionInput: string='';
   precioInput: number=0;
+  valores: boolean=false;
+  bol2: Boolean=true;
 
   articulos: Articulo[]= [ new Articulo(1,'papas',1500)];
 
   agregarArticulo(){
-    let articuloN=new Articulo(this.codigoInput, this.descripcionInput, this.precioInput);
-    this.articulos.push(articuloN)
+    this.verificarValores(true);
+    if(this.valores){
+      let articuloN=new Articulo(this.codigoInput, this.descripcionInput, this.precioInput);
+      this.articulos.push(articuloN)
+    }
+
   }
 
   borrarArticulo(codigoB: number){
@@ -25,5 +31,63 @@ export class AppComponent {
         obj.splice(index,1);
       }
     })
+  }
+
+  seleccionarArticulo(codigoS: number){
+    this.articulos.forEach( (articuloB, index, obj) =>{
+      if(articuloB.codigo === codigoS){
+        this.codigoInput = codigoS;
+        this.descripcionInput = articuloB.descripcion;
+        this.precioInput = articuloB.precio;
+      }
+    })
+  }
+
+  modificarArticulo(){
+    this.verificarValores(false);
+    if(this.valores){
+      this.articulos.forEach( (articuloB, index, obj) =>{
+        if(articuloB.codigo === this.codigoInput){
+          console.log(this.articulos[index].codigo);
+          this.articulos[index].descripcion=this.descripcionInput;
+          this.articulos[index].precio=this.precioInput;
+        }
+      })
+    }
+
+  }
+
+  verificarValores(bol: boolean){
+    if (this.codigoInput <= 0){
+      alert("Codigo incorrecto");
+    }else if(this.descripcionInput == ''){
+      alert("Descripcion no puede estar vacia");
+    }else if (this.precioInput < 50){
+      alert("Precio incorrecto");
+    }else{
+      this.valores=true;
+      if(bol){
+        this.articulos.forEach((articuloB) =>{
+          if (articuloB.codigo === this.codigoInput){
+            alert("Codigo Repetido");
+            this.valores=false;
+          }
+        })
+      }else{
+        this.articulos.forEach((articuloB) =>{
+          if(this.bol2){
+            if (articuloB.codigo === this.codigoInput){
+              this.valores=true;
+              this.bol2=false;
+            }else{
+              this.valores=false;
+            }
+          }
+        })
+        if(!(this.valores)){
+          alert("Codigo no existe");
+        }
+      }
+    }
   }
 }
